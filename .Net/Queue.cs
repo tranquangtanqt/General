cbf32bca51bd292e5d99397709510fa59b890878
tranquangtanqt <tranquangtanqt1990@gmail.com>
2020-11-18 22:09:42 +0700

    add .net queue

---
+++ .Net/Queue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace DemoQueue
+{
+    class Program
+    {
+        public const string PATTENRN_FILE = "^Infile\\d*";
+
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue("Infile1");
+            queue.Enqueue("-");
+            queue.Enqueue("INFILE");
+            queue.Enqueue("Outfile");
+            queue.Enqueue("-");
+            queue.Enqueue("OUTFILE");
+
+            while(queue.Count > 0)
+            {
+                String str = (String) queue.Dequeue();
+                if (Regex.IsMatch(str, PATTENRN_FILE))
+                {
+                    if ("-".Equals(queue.Dequeue()))
+                    {
+                        Console.WriteLine("FILE-" + queue.Dequeue());
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
